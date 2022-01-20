40aec492feeda94b4ce32fc452ccabbe66bb102b
amandagabriela74 <97493028+amandagabriela74@users.noreply.github.com>
2022-01-20 18:32:35 -0300

    Add files via upload

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,134 @@
 <script>
-	import Menu from "./Menu.svelte";
-	import Sobre from "./Sobre.svelte";
-	import Ajuda from "./Ajuda.svelte";
+	import Contatos from './componentes/Contatos.svelte';
+	import Botao from './componentes/Botao.svelte';
+	import Sobre from './componentes/Sobre.svelte';
+	import Ajuda from './componentes/Ajuda.svelte';
+	import JogoDaVelha9 from "./componentes/Jogo-da-velha9.svelte";
+	import JogoDaVelha from "./componentes/Jogo-da-velha.svelte";
+
+	let menu = 0;
 
 
-	import { estado } from './Estado.js'
+	function jogarnove() {
+		var x = document.getElementById("9casas");
+		var y = document.getElementById("16casas");
+
+		if (x.style.display === "none") {
+			x.style.display = "block";
+			y.style.display = "none";
+		
+		} else {
+			x.style.display = "none";
+		}
+	}
+
+	function jogardez(){
+		var y = document.getElementById("16casas");
+		var x = document.getElementById("9casas");
+
+		if (y.style.display === "none") {
+			y.style.display = "block";
+			x.style.display = "none";
+		} else {
+			y.style.display = "none";
+		}
+	}
+
+
 </script>
 
+<ul id="menu">
+	<li class="menuu" ><a href="/" on:click|preventDefault={() => (menu = 0)}>𝒥𝑜𝑔𝒶𝓇</a></li> 
+	<li class="menuu"><a href="/" on:click|preventDefault={() => (menu = 1)}>𝒮𝑜𝒷𝓇𝑒</a></li>  
+	<li class="menuu"><a href="/" on:click|preventDefault={() => (menu = 2)}>𝒜𝒿𝓊𝒹𝒶</a></li> 
+	<li class="menuu"><a href="/" on:click|preventDefault={() => (menu = 3)}>Contatos</a></li> 
+</ul>
 
-<!-- o jogo tem três estados menu, jogar e sobre, cada um representa uma tela diferente para o usuário -->
-{#if $estado === 'menu'}
-	<!-- notem como cada componente/módulo que crianos no svelte vira um tag -->
-	<Menu/>
-{:else if $estado === 'sobre'}
-	<Sobre/>
-{:else if $estado === 'ajuda'}
-	<Ajuda/>
+{#if menu === 0}
+<div class="botoesmudar">
+	<button on:click={jogarnove}>Jogar com 9 casas.</button>
+	<button on:click={jogardez}>Jogar com 16 casas.</button>
+</div>
+<main>
+	<div class="acasas"   id="9casas"> 
+		<JogoDaVelha9/>
+	</div>
+	<div class="ycasas" id="16casas">
+		<JogoDaVelha/>
+	</div>
+</main>
+{:else if menu === 1}
+<Sobre/>
+{:else if menu === 2}
+<Ajuda />
+{:else if menu === 3}
+<Contatos/>
 {/if}
+
+<style>
+	.menuu{
+		margin-left: 15px;
+	}
+
+.botoesmudar{
+	text-align: center;
+	margin-top: 20px;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0px;
+  padding: 0;
+  overflow: hidden;
+	background: linear-gradient(90deg, rgba(18,33,80,1) 5%, rgba(38,54,147,1) 50%, rgba(21,39,96,1) 95%);
+
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 1px 18px;
+  text-decoration: none;
+	margin-left: 5px;
+	border: solid;
+	border-style: groove;
+	border-radius: 5px;
+	background: linear-gradient(90deg, rgba(6,0,96,1) 22%, rgba(26,49,103,1) 43%, rgba(9,12,24,1) 100%);
+}
+
+
+.ycasas{
+	display: none;
+	
+}
+
+.acasas{
+	display: none;
+	text-align: center;
+}
+
+	:global(body) {
+		background: linear-gradient(90deg, rgba(18,33,80,1) 5%, rgba(38,54,147,1) 50%, rgba(21,39,96,1) 95%);
+	}
+	main {
+		margin-top: 50px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		line-height: 10px;
+	}
+
+	button {
+		font-size: 0.4cm;
+		background: linear-gradient(90deg, rgba(6,0,96,1) 22%, rgba(26,49,103,1) 43%, rgba(9,12,24,1) 100%);
+		margin: 1px;
+		color: aliceblue;
+		font-family: 'Roboto', sans-serif;
+	 
+	}
+</style>
---
+++ src/componentes/Contatos.svelte
@@ -0,0 +1,124 @@
+<html lang="pt-br">
+  <body>
+    <main>
+      <div>
+        <section>
+          <h1>Contatos</h1>
+          <p class="texto">Caso tenha dúvidas ou sugestões de melhorias sobre o jogo, pode escolher  qualquer um dos desenvolvedores abaixo, que responderemos o mais breve possível. Será um prazer entrar em contato com você!</p>
+          <br>
+        </section>
+        <div class="coluna">
+          <div class="bloco">
+            <img class="perfil" src="amanda.jpeg" alt="" />
+            <div>
+              <p>Amanda Gabriela da Silva Oliveira</p>
+              <br>
+              <a href="https://www.linkedin.com/in/amanda-gabriela-a4a926200/"><img class="imagem" src="linkedin.png" alt="Linkedin"/></a>
+              <a href="https://www.instagram.com/amandagabriela74/"><img class="imagem" src="instagram.png" alt="Instagram" /></a>
+              <a href="https://github.com/amandagabriela74"><img class="imagem" src="github.png" alt="Github" /></a>
+            </div>
+          </div>
+          <div class="bloco">
+            <img class="perfil" src="andry.jpeg" alt="" />
+            <div>
+              <p>Andrey Santana Mafra Guerra</p>
+              <br>
+              <a href=" "><img class="imagem" src="linkedin.png" alt="Linkedin"/></a>
+              <a href=" "><img class="imagem" src="instagram.png" alt="Instagram"/></a>
+              <a href=" "><img class="imagem" src="github.png" alt="Github"/></a>
+            </div>
+          </div>
+        </div>
+        <div class="coluna">
+          <div class="bloco">
+            <img class="perfil" src="bruno.jpeg" alt="" />
+            <div>
+              <p>Bruno imagem Crispim da Silva</p>
+              <br>
+              <a href="https://www.linkedin.com/in/bruno-wilian-317066192/"><img class="imagem" src="linkedin.png" alt=""/></a>
+              <a href="https://www.instagram.com/bruno_.wilian/"><img class="imagem" src="instagram.png" alt="Instagram" /> </a>
+              <a href="https://github.com/brunowilian"><img class="imagem"  src="Github.png" alt="Github" /></a>
+            </div>
+          </div>
+          <div class="bloco">
+            <img class="perfil" src="saara.jpeg" alt="" />
+            <div>
+              <p>Daniele Saara dos Santos</p>
+              <br>
+              <a href=" "><img class="imagem" src="linkedin.png" alt="" /></a>
+                <a href="https://www.instagram.com/daniele.saara"><img class="imagem" src="instagram.png" alt="" /></a>
+                  <a href="https://github.com/danielesaara"><img class="imagem" src="github.png" alt="" /></a>
+            </div>
+          </div>
+        </div>
+        <div class="coluna">
+          <div class="bloco">
+            <img class="perfil" src="flavio.jpeg" alt="" />
+            <div>
+              <p>Flávio de Lima Santos</p>
+              <br>
+              
+              <a href=" " ><img class="imagem" src="linkedin.png" alt=""/></a>
+                <a href="https://www.instagram.com/flaviolima_23"><img class="imagem" src="instagram.png" alt=""/></a>
+                  <a href="https://github.com/Flaviolima719"><img class="imagem" src="github.png" alt=""/></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </body>
+</html>
+
+<style>
+  .texto{
+    color: white;
+    font-size: 20px;
+    margin-left: 40px;
+  }
+  .coluna {
+    display: flex;
+    flex-direction: row;
+    margin-top: 10px;
+    margin-left:25px;
+    margin-bottom: 10px;
+  }
+  h1 {
+    text-align: center;
+    color: white;
+  }
+  main {
+    background-color: rgba(0, 0, 0, 0.534);
+    padding: 2px 10px;
+    margin-top: 20px;
+    margin-right: 20%;
+    margin-left: 20%;
+    border-radius: 10px;
+    font-family: "Roboto", sans-serif;
+  }
+
+  .perfil {
+    border-radius: 10%;
+    height: 8rem;
+    width: 8rem;
+    margin: 15px;
+  
+  }
+
+  .bloco {
+    display: flex;
+    flex-direction: row;
+    margin-top: 10px;
+    border: solid;
+    color: white;
+    width: 9cm;
+    margin: 0%;
+    margin-left: 10px;
+  }
+  .imagem {
+    border-radius: 50%;
+    height: 25px;
+    width: 25px;
+    margin: 0px;
+    margin-left: 10px;
+  }
+</style>
